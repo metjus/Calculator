bcb5d9829351a2b55b757d223a39bf2328683724
metjus <matus.sturdik1@gmail.com>
2016-09-07 17:38:25 +0200

    Delete and Edit names of some functions.

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
- var a= 0;
- var b = 0;
- var x= 0;
-
-
 
 function result(){
-  document.getElementById('result').innerHTML=x;
+  
 }
 
 function add(a, b) {
@@ -24,20 +19,8 @@ function divide(a, b) {
   return a/b;
 }
 
-function ac(){
+function AllClear(){
     a=2;
     b=2;
     document.getElementById('result').innerHTML = 0;
   }
-
-function n1() {
-  if (a === 0) {
-    a=a+1;
-    document.getElementById('result').innerHTML = 1;
-  }
-  else {
-    b=b+1;
-    document.getElementById('result').innerHTML = 1;
-  }
-
-}
